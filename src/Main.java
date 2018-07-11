b242ec0b781eb25a67db698c74d06e321f39ae20
tsigereda-affa <affa16.a@gmail.com>
2018-07-11 16:43:31 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Box");
+        System.out.println("*****");
+        System.out.println("*\t*");
+        System.out.println("*\t*");
+        System.out.println("*\t*");
+        System.out.println("*\t*");
+        System.out.println("*****");
+        System.out.println("Diamond");
+        System.out.println("\t\t*");
+        System.out.println("\t*\t\t*");
+        System.out.println("\b*\t\t\t\t*");
+        System.out.println("\t*\t\t*");
+        System.out.println("\t\t*");
+
+            }
+}
